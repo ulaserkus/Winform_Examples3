02d9254b57006c2813bcba14d69b7ccc18637a40
Ulaş Hasan <67653664+ulaserkus@users.noreply.github.com>
2021-01-14 14:22:16 +0200

    Add files via upload

---
+++ ChangeOffice/ChangeOffice/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace ChangeOffice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_buy_dolar = new System.Windows.Forms.Label();
+            this.lbl_sell_dolar = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_buy_euro = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_sell_euro = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_amount = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txt_quantity = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_exchange_rate = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txt_remaining = new System.Windows.Forms.TextBox();
+            this.btn_buy = new System.Windows.Forms.Button();
+            this.btn_buy_all = new System.Windows.Forms.Button();
+            this.btn_buy_dollar = new System.Windows.Forms.Button();
+            this.btn_buy_euro = new System.Windows.Forms.Button();
+            this.btn_sell_dollar = new System.Windows.Forms.Button();
+            this.btn_sell_euro = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dollar purchase price";
+            // 
+            // lbl_buy_dolar
+            // 
+            this.lbl_buy_dolar.AutoSize = true;
+            this.lbl_buy_dolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_buy_dolar.Location = new System.Drawing.Point(197, 112);
+            this.lbl_buy_dolar.Name = "lbl_buy_dolar";
+            this.lbl_buy_dolar.Size = new System.Drawing.Size(19, 20);
+            this.lbl_buy_dolar.TabIndex = 1;
+            this.lbl_buy_dolar.Text = "0";
+            // 
+            // lbl_sell_dolar
+            // 
+            this.lbl_sell_dolar.AutoSize = true;
+            this.lbl_sell_dolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sell_dolar.Location = new System.Drawing.Point(174, 159);
+            this.lbl_sell_dolar.Name = "lbl_sell_dolar";
+            this.lbl_sell_dolar.Size = new System.Drawing.Size(19, 20);
+            this.lbl_sell_dolar.TabIndex = 3;
+            this.lbl_sell_dolar.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Dollar selling price";
+            // 
+            // lbl_buy_euro
+            // 
+            this.lbl_buy_euro.AutoSize = true;
+            this.lbl_buy_euro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_buy_euro.Location = new System.Drawing.Point(188, 206);
+            this.lbl_buy_euro.Name = "lbl_buy_euro";
+            this.lbl_buy_euro.Size = new System.Drawing.Size(19, 20);
+            this.lbl_buy_euro.TabIndex = 5;
+            this.lbl_buy_euro.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(12, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Euro purchase price";
+            // 
+            // lbl_sell_euro
+            // 
+            this.lbl_sell_euro.AutoSize = true;
+            this.lbl_sell_euro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sell_euro.Location = new System.Drawing.Point(165, 257);
+            this.lbl_sell_euro.Name = "lbl_sell_euro";
+            this.lbl_sell_euro.Size = new System.Drawing.Size(19, 20);
+            this.lbl_sell_euro.TabIndex = 7;
+            this.lbl_sell_euro.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(12, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 20);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Euro selling price";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_buy_all);
+            this.groupBox1.Controls.Add(this.btn_buy);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.txt_remaining);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.txt_amount);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txt_quantity);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txt_exchange_rate);
+            this.groupBox1.Location = new System.Drawing.Point(351, -1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 427);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(54, 168);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(63, 20);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "amount";
+            // 
+            // txt_amount
+            // 
+            this.txt_amount.Location = new System.Drawing.Point(161, 165);
+            this.txt_amount.Name = "txt_amount";
+            this.txt_amount.Size = new System.Drawing.Size(145, 26);
+            this.txt_amount.TabIndex = 4;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(54, 109);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 20);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "quantity";
+            // 
+            // txt_quantity
+            // 
+            this.txt_quantity.Location = new System.Drawing.Point(161, 106);
+            this.txt_quantity.Name = "txt_quantity";
+            this.txt_quantity.Size = new System.Drawing.Size(145, 26);
+            this.txt_quantity.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(45, 43);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 20);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "exchange rate";
+            // 
+            // txt_exchange_rate
+            // 
+            this.txt_exchange_rate.Location = new System.Drawing.Point(161, 40);
+            this.txt_exchange_rate.Name = "txt_exchange_rate";
+            this.txt_exchange_rate.Size = new System.Drawing.Size(145, 26);
+            this.txt_exchange_rate.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(56, 229);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(78, 20);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "remaining";
+            // 
+            // txt_remaining
+            // 
+            this.txt_remaining.Location = new System.Drawing.Point(161, 226);
+            this.txt_remaining.Name = "txt_remaining";
+            this.txt_remaining.Size = new System.Drawing.Size(145, 26);
+            this.txt_remaining.TabIndex = 6;
+            // 
+            // btn_buy
+            // 
+            this.btn_buy.Location = new System.Drawing.Point(49, 293);
+            this.btn_buy.Name = "btn_buy";
+            this.btn_buy.Size = new System.Drawing.Size(97, 70);
+            this.btn_buy.TabIndex = 8;
+            this.btn_buy.Text = "Buy";
+            this.btn_buy.UseVisualStyleBackColor = true;
+            this.btn_buy.Click += new System.EventHandler(this.btn_buy_Click);
+            // 
+            // btn_buy_all
+            // 
+            this.btn_buy_all.Location = new System.Drawing.Point(209, 293);
+            this.btn_buy_all.Name = "btn_buy_all";
+            this.btn_buy_all.Size = new System.Drawing.Size(97, 70);
+            this.btn_buy_all.TabIndex = 9;
+            this.btn_buy_all.Text = "Buy All";
+            this.btn_buy_all.UseVisualStyleBackColor = true;
+            this.btn_buy_all.Click += new System.EventHandler(this.btn_buy_all_Click);
+            // 
+            // btn_buy_dollar
+            // 
+            this.btn_buy_dollar.Location = new System.Drawing.Point(265, 105);
+            this.btn_buy_dollar.Name = "btn_buy_dollar";
+            this.btn_buy_dollar.Size = new System.Drawing.Size(54, 27);
+            this.btn_buy_dollar.TabIndex = 9;
+            this.btn_buy_dollar.Text = "Al";
+            this.btn_buy_dollar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_buy_dollar.UseVisualStyleBackColor = true;
+            this.btn_buy_dollar.Click += new System.EventHandler(this.btn_buy_dollar_Click);
+            // 
+            // btn_buy_euro
+            // 
+            this.btn_buy_euro.Location = new System.Drawing.Point(265, 206);
+            this.btn_buy_euro.Name = "btn_buy_euro";
+            this.btn_buy_euro.Size = new System.Drawing.Size(54, 27);
+            this.btn_buy_euro.TabIndex = 10;
+            this.btn_buy_euro.Text = "Al";
+            this.btn_buy_euro.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_buy_euro.UseVisualStyleBackColor = true;
+            this.btn_buy_euro.Click += new System.EventHandler(this.btn_buy_euro_Click);
+            // 
+            // btn_sell_dollar
+            // 
+            this.btn_sell_dollar.Location = new System.Drawing.Point(265, 157);
+            this.btn_sell_dollar.Name = "btn_sell_dollar";
+            this.btn_sell_dollar.Size = new System.Drawing.Size(54, 27);
+            this.btn_sell_dollar.TabIndex = 11;
+            this.btn_sell_dollar.Text = "Sat";
+            this.btn_sell_dollar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_sell_dollar.UseVisualStyleBackColor = true;
+            this.btn_sell_dollar.Click += new System.EventHandler(this.btn_sell_dollar_Click);
+            // 
+            // btn_sell_euro
+            // 
+            this.btn_sell_euro.Location = new System.Drawing.Point(265, 257);
+            this.btn_sell_euro.Name = "btn_sell_euro";
+            this.btn_sell_euro.Size = new System.Drawing.Size(54, 27);
+            this.btn_sell_euro.TabIndex = 12;
+            this.btn_sell_euro.Text = "Sat";
+            this.btn_sell_euro.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_sell_euro.UseVisualStyleBackColor = true;
+            this.btn_sell_euro.Click += new System.EventHandler(this.btn_sell_euro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(711, 422);
+            this.Controls.Add(this.btn_sell_euro);
+            this.Controls.Add(this.btn_sell_dollar);
+            this.Controls.Add(this.btn_buy_euro);
+            this.Controls.Add(this.btn_buy_dollar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_sell_euro);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl_buy_euro);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_sell_dolar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_buy_dolar);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_buy_dolar;
+        private System.Windows.Forms.Label lbl_sell_dolar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_buy_euro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_sell_euro;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_amount;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txt_quantity;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_exchange_rate;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txt_remaining;
+        private System.Windows.Forms.Button btn_buy_all;
+        private System.Windows.Forms.Button btn_buy;
+        private System.Windows.Forms.Button btn_buy_dollar;
+        private System.Windows.Forms.Button btn_buy_euro;
+        private System.Windows.Forms.Button btn_sell_dollar;
+        private System.Windows.Forms.Button btn_sell_euro;
+    }
+}
+
---
+++ ChangeOffice/ChangeOffice/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ChangeOffice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string today = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmlFile = new XmlDocument();
+            xmlFile.Load(today);
+
+            string dolarBuy = xmlFile.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            lbl_buy_dolar.Text = dolarBuy;
+
+            string dolarSell = xmlFile.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            lbl_sell_dolar.Text = dolarSell;
+
+
+            string euroBuy = xmlFile.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            lbl_buy_euro.Text = euroBuy;
+
+            string euroSell = xmlFile.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+           lbl_sell_euro.Text = euroSell;
+
+
+        }
+
+        private void btn_buy_dollar_Click(object sender, EventArgs e)
+        {
+            txt_exchange_rate.Text = lbl_buy_dolar.Text;
+        }
+
+        private void btn_sell_dollar_Click(object sender, EventArgs e)
+        {
+            txt_exchange_rate.Text = lbl_sell_dolar.Text;
+        }
+
+        private void btn_buy_euro_Click(object sender, EventArgs e)
+        {
+            txt_exchange_rate.Text = lbl_buy_euro.Text;
+        }
+
+        private void btn_sell_euro_Click(object sender, EventArgs e)
+        {
+            txt_exchange_rate.Text = lbl_sell_euro.Text;
+        }
+
+        private void btn_buy_Click(object sender, EventArgs e)
+        {
+            double changeRate, quantity, amount;
+            changeRate = Convert.ToDouble(txt_exchange_rate.Text);
+            quantity = Convert.ToDouble(txt_quantity.Text);
+            amount = quantity * changeRate;
+            txt_amount.Text = amount.ToString();
+        }
+
+        private void btn_buy_all_Click(object sender, EventArgs e)
+        {
+            double changeRate = Convert.ToDouble(txt_exchange_rate.Text);
+            double quantity = Convert.ToDouble(txt_quantity.Text);
+
+            double amount= (quantity / changeRate);
+            txt_amount.Text = amount.ToString();
+
+            double remaining = quantity % changeRate;
+            txt_remaining.Text = remaining.ToString();
+
+            
+        }
+    }
+}
